73aa1e8c223849d53781616cd1522ead9aed054e
TrongTins <vucongao@gmail.com>
2025-05-13 16:41:37 +0700

    feat: update index screen

--- lib/home/widgets/select_priority_dialog.dart
+++ lib/home/widgets/select_priority_dialog.dart
@@ -39,7 +39,7 @@ class _SelectPriorityDialogState extends State<SelectPriorityDialog> {
               'Task Priority',
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 16,
+                fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
