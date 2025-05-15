9449b76f36a169fac182a02b50dfbe42e3a37137
vinhdevz <vinhdev.pixel@gmail.com>
2025-05-15 09:09:00 +0700

    feat(updateprofile): create feat/updateprofile

--- lib/database/task_database.dart
+++ lib/database/task_database.dart
@@ -1,4 +1,4 @@
-import 'package:sqflite/sqflite.dart'; 
+import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 import '../models/task_model.dart';
@@ -18,7 +18,17 @@ class TaskDatabase {
   Future<Database> _initDB(String filePath) async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
-    return await openDatabase(path, version: 1, onCreate: _createDB);
+    return await openDatabase(
+      path,
+      version: 2,
+      onCreate: _createDB,
+      onUpgrade: (db, oldVersion, newVersion) async {
+        if (oldVersion < 2) {
+          await db.execute(
+              'ALTER TABLE tasks ADD COLUMN isCompleted INTEGER NOT NULL DEFAULT 0');
+        }
+      },
+    );
   }
 
   Future _createDB(Database db, int version) async {
@@ -65,14 +75,14 @@ class TaskDatabase {
     db.close();
   }
 
-  
   Future<Map<String, int>> loadTaskStats() async {
     final db = await instance.database;
 
-    
-    final completedResult = await db.rawQuery('SELECT COUNT(*) FROM tasks WHERE isCompleted = 1');
-    final uncompletedResult = await db.rawQuery('SELECT COUNT(*) FROM tasks WHERE isCompleted = 0');
-    
+    final completedResult =
+        await db.rawQuery('SELECT COUNT(*) FROM tasks WHERE isCompleted = 1');
+    final uncompletedResult =
+        await db.rawQuery('SELECT COUNT(*) FROM tasks WHERE isCompleted = 0');
+
     final completed = Sqflite.firstIntValue(completedResult) ?? 0;
     final uncompleted = Sqflite.firstIntValue(uncompletedResult) ?? 0;
 
